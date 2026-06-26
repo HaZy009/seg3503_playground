b8547b632818153fe372eab48ce593a53b2b0774
HaZy009 <joelnormand7@gmail.com>
2026-06-26 15:58:39 -0400

    Reorganise folder lab 4 & add Junit in it to run test. Project is working and is ready to start the lab4

---
+++ lab04/tic_java/test/tic/TicTest.java
@@ -0,0 +1,22 @@
+package tic;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TicTest {
+
+    @Test
+    void shouldCreateEmpty3x3Board() {
+        Tic board = new Tic(3, 3);
+
+        assertEquals(3, board.rows);
+        assertEquals(3, board.cols);
+        assertEquals("X", board.turn);
+
+        for (int i = 0; i < board.rows; i++) {
+            for (int j = 0; j < board.cols; j++) {
+                assertEquals("_", board.board[i][j]);
+            }
+        }
+    }
+}
